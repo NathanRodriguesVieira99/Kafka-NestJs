e0c5b5c4154879058ea9c788f635c0f28aff5aa9
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2025-12-30 01:27:10 -0300

    refactor(http): remove HttpModule

--- apps/orders/src/infra/http/http.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OrdersController } from './controllers/orders.controller';
-import { OrdersService } from '../../domain/services/orders.service';
-
-@Module({
-  providers: [OrdersService],
-  controllers: [OrdersController],
-})
-export class HttpModule {}
