c053194bc0a9d4c4bc1f25eed6631510b08a99be
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-01-02 17:49:02 -0300

    test(e2e): add initial end-to-end tests for OrdersController

---
+++ apps/orders/src/infra/http/controllers/orders.controller.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { Test } from '@nestjs/testing';
+import { PrismaService } from '../../database/prisma.service';
+import type { INestApplication } from '@nestjs/common';
+import { OrdersModule } from 'apps/orders/src/orders.module';
+import request from 'supertest';
+
+describe('OrdersController (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  // cria o App e se conecta ao db apenas uma vez
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [OrdersModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+
+    prisma = moduleRef.get<PrismaService>(PrismaService);
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect(); // Fecha a conexão com o Prisma
+    await app.close(); // Fecha a conexão com o App
+  });
+
+  beforeEach(async () => {
+    await prisma.order.deleteMany(); // limpa o banco antes de cada teste e2e
+  });
+
+  it('should be defined', () => {
+    expect(app).toBeDefined();
+  });
+
+  it('[GET] /orders - should return all orders', async () => {
+    const res = await request(app.getHttpServer()).get('/orders');
+
+    expect(res.statusCode).toBe(200);
+  });
+});
