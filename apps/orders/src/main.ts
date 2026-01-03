601ed23c23514c12a277e54cc5353fd4b9cb2ab6
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-01-03 15:50:00 -0300

    feat(orders): configure Kafka microservice for Orders application

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { OrdersModule } from './orders.module';
 import { env } from './env';
+import { Transport, type MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'], // 29092 se estiver dentro do docker
+      },
+      consumer: { groupId: 'orders-consumer' },
+    },
+  });
+
+  await app.startAllMicroservices();
+
   await app.listen(env.PORT);
 }
 bootstrap();
