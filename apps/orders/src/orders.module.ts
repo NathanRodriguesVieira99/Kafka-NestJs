045b7456cfc1390051436655814e2a829e913404
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-01-03 15:49:51 -0300

    feat(orders): integrate Kafka transport for Orders service

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,16 +1,31 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './infra/database/prisma.module';
-import { HttpModule } from './infra/http/http.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { OrdersService } from './domain/services/orders/orders.service';
+import { OrdersController } from './infra/http/controllers/orders.controller';
 
 @Module({
   imports: [
+    PrismaModule,
+    ClientsModule.register([
+      {
+        name: 'ORDERS_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'orders',
+            brokers: ['localhost:9092'], // 29092 se estiver dentro do docker
+          },
+        },
+      },
+    ]),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
-    PrismaModule,
-    HttpModule,
   ],
+  providers: [OrdersService],
+  controllers: [OrdersController],
 })
 export class OrdersModule {}
